b4d49aadc7a2b22cdb4c0b6973f281288516ba44
Umair Idris <umairidris@google.com>
2019-11-13 21:49:15 +0000

    add initial implementation and corresponding variables

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018 Google LLC
+ * Copyright 2019 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,69 @@
  * limitations under the License.
  */
 
-variable "project_id" {
-  description = "The project ID to deploy to"
+variable "name" {
+    type = string
+    description = "The resource name for the Dataset.s"
 }
 
-variable "bucket_name" {
-  description = "The name of the bucket to create"
+variable "project" {
+    type = string
+    description = "The ID of the project in which the resource belongs. If it is not provided, the provider project is used."
+    default = null
+}
+
+variable "location" {
+    type = string
+    description = "The location for the Dataset."
+}
+
+variable "time_zone" {
+    type = string
+    description = "The default timezone used by this dataset."
+    default = null
+}
+
+variable "iam_members" {
+    type = list(object({
+        role = string
+        member = string
+    }))
+    description = "Updates the IAM policy to grant a role to a new member. Other members for the role for the dataset are preserved."
+    default = []
+}
+
+variable "dicom_stores" {
+    type = list(object({
+        name = string
+        iam_members = list(object({
+            role = string
+            member = string
+        }))
+    }))
+    description = "Datastore that conforms to the DICOM (https://www.dicomstandard.org/about/) standard for Healthcare information exchange."
+    default = []
+}
+
+variable "fhir_stores" {
+    type = list(object({
+        name = string
+        iam_members = list(object({
+            role = string
+            member = string
+        }))
+    }))
+    description = "Datastore that conforms to the FHIR (https://www.hl7.org/fhir/STU3/) standard for Healthcare information exchange."
+    default = []
+}
+
+variable "hl7_v2_stores" {
+    type = list(object({
+        name = string
+        iam_members = list(object({
+            role = string
+            member = string
+        }))
+    }))
+    description = "Datastore that conforms to the HL7 V2 ((https://www.hl7.org/hl7V2/STU3/) standard for Healthcare information exchange."
+    default = []
 }
