a308f52c5b1b49d8b0fac75058048353ccc43638
Umair Idris <umairidris@google.com>
2019-11-14 02:22:30 +0000

    use store name over id in store iam member resource names

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ locals {
     all_dicom_iam_members = flatten([
         for s in var.dicom_stores: [
             for m in s.iam_members: {
-                store_id = google_healthcare_dicom_store.dicom_stores[s.name].id
+                store_name = s.name
                 role = m.role
                 member = m.member
             }
@@ -76,7 +76,7 @@ locals {
     all_fhir_iam_members = flatten([
         for s in var.fhir_stores: [
             for m in s.iam_members: {
-                store_id = google_healthcare_fhir_store.fhir_stores[s.name].id
+                store_name = s.name
                 role = m.role
                 member = m.member
             }
@@ -85,7 +85,7 @@ locals {
     all_hl7_v2_iam_members = flatten([
         for s in var.hl7_v2_stores: [
             for m in s.iam_members: {
-                store_id = google_healthcare_hl7_v2_store.hl7_v2_stores[s.name].id
+                store_name = s.name
                 role = m.role
                 member = m.member
             }
@@ -97,9 +97,9 @@ resource "google_healthcare_dicom_store_iam_member" "dicom_store_iam_members" {
     provider = google-beta
     for_each = {
         for m in local.all_dicom_iam_members:
-            "${m.store_id} ${m.role} ${m.member}" => m
+            "${m.store_name} ${m.role} ${m.member}" => m
     }
-    dicom_store_id = each.value.store_id
+    dicom_store_id = google_healthcare_dicom_store.dicom_stores[each.value.store_name].id
     role = each.value.role
     member = each.value.member
 }
@@ -108,9 +108,9 @@ resource "google_healthcare_fhir_store_iam_member" "fhir_store_iam_members" {
     provider = google-beta
     for_each = {
         for m in local.all_fhir_iam_members:
-            "${m.store_id} ${m.role} ${m.member}" => m
+            "${m.store_name} ${m.role} ${m.member}" => m
     }
-    fhir_store_id = each.value.store_id
+    fhir_store_id = google_healthcare_fhir_store.fhir_stores[each.value.store_name].id
     role = each.value.role
     member = each.value.member
 }
@@ -119,9 +119,9 @@ resource "google_healthcare_hl7_v2_store_iam_member" "hl7_v2_store_iam_members" 
     provider = google-beta
     for_each = {
         for m in local.all_hl7_v2_iam_members:
-            "${m.store_id} ${m.role} ${m.member}" => m
+            "${m.store_name} ${m.role} ${m.member}" => m
     }
-    hl7_v2_store_id = each.value.store_id
+    hl7_v2_store_id = google_healthcare_hl7_v2_store.hl7_v2_stores[each.value.store_name].id
     role = each.value.role
     member = each.value.member
 }
