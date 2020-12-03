f2a8c6dc1d61f05ae2303ebb1f1e0f6975f0b17a
Xin Gao <xingao@google.com>
2020-12-03 15:59:02 -0500

    skip gcloud download

--- test/setup/main.tf
+++ test/setup/main.tf
@@ -18,11 +18,13 @@ module "project" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 9.2.0"
 
-  name              = "ci-healthcare"
-  random_project_id = "true"
-  org_id            = var.org_id
-  folder_id         = var.folder_id
-  billing_account   = var.billing_account
+  name                 = "ci-healthcare"
+  random_project_id    = "true"
+  org_id               = var.org_id
+  folder_id            = var.folder_id
+  billing_account      = var.billing_account
+  skip_gcloud_download = true
+
 
   activate_apis = [
     "healthcare.googleapis.com",
