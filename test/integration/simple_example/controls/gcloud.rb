05b7757ef72625af836d2c3b2907fb7151b721b9
Xin Gao <xingao267@users.noreply.github.com>
2020-12-04 11:45:54 -0500

    chore: upgrade test infra and add more tests (#29)
    * chore: upgrade test infra and add more tests
    
    Fixes #23

--- test/integration/simple_example/controls/gcloud.rb
+++ test/integration/simple_example/controls/gcloud.rb
@@ -18,59 +18,60 @@ control "gcloud" do
   describe command("gcloud --project=#{attribute("project")} services list --enabled") do
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq "" }
-    its(:stdout) { should match "healthcare.googleapis.com" }
+    its(:stdout) { should include "healthcare.googleapis.com" }
   end
 
   # Datasets
   describe command("gcloud --project=#{attribute("project")} healthcare datasets list") do
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq "" }
-    its(:stdout) { should match "example-healthcare-dataset" }
+    its(:stdout) { should include "example-healthcare-dataset" }
   end
 
   # DICOM stores
   describe command("gcloud --project=#{attribute("project")} healthcare dicom-stores list --dataset=example-healthcare-dataset") do
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq "" }
-    its(:stdout) { should match "example-dicom-a" }
-    its(:stdout) { should match "example-dicom-b" }
+    its(:stdout) { should include "example-dicom-a" }
+    its(:stdout) { should include "example-dicom-b" }
   end
 
   # FHIR stores
   describe command("gcloud --project=#{attribute("project")} healthcare fhir-stores list --dataset=example-healthcare-dataset") do
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq "" }
-    its(:stdout) { should match "example-fhir" }
+    its(:stdout) { should include "example-fhir" }
   end
 
   # HL7V2 Stores
   describe command("gcloud --project=#{attribute("project")} healthcare hl7v2-stores list --dataset=example-healthcare-dataset") do
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq "" }
-    its(:stdout) { should match "example-hl7v2" }
+    its(:stdout) { should include "example-hl7v2" }
   end
 
   describe command("gcloud --project=#{attribute("project")} healthcare dicom-stores describe example-dicom-a --dataset=example-healthcare-dataset") do
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq "" }
-    its(:stdout) { should match "projects/#{attribute("project")}/topics/example-topic\n" }
+    its(:stdout) { should include "notificationConfig" }
+    its(:stdout) { should include "projects/#{attribute("project")}/topics/example-topic\n" }
   end
 
   describe command("gcloud --project=#{attribute("project")} healthcare fhir-stores describe example-fhir --dataset=example-healthcare-dataset") do
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq "" }
-    its(:stdout) { should match "projects/#{attribute("project")}/topics/example-topic\n" }
+    its(:stdout) { should include "notificationConfig" }
+    its(:stdout) { should include "projects/#{attribute("project")}/topics/example-topic\n" }
+    its(:stdout) { should include "streamConfigs" }
+    its(:stdout) { should include "bq://#{attribute("project")}.example_dataset" }
+    its(:stdout) { should include "recursiveStructureDepth: '3'" }
   end
 
-  describe command("gcloud --project=#{attribute("project")} healthcare hl7v2-stores describe example-hl7v2 --dataset=example-healthcare-dataset") do
+  describe command("gcloud beta --project=#{attribute("project")} healthcare hl7v2-stores describe example-hl7v2 --dataset=example-healthcare-dataset") do
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq "" }
-    its(:stdout) { should match "projects/#{attribute("project")}/topics/example-topic\n" }
-  end
-
-  describe command("gcloud --project=#{attribute("project")} beta healthcare hl7v2-stores describe example-hl7v2 --dataset=example-healthcare-dataset") do
-    its(:exit_status) { should eq 0 }
-    its(:stderr) { should eq "" }
+    its(:stdout) { should include "notificationConfigs" }
+    its(:stdout) { should include "projects/#{attribute("project")}/topics/example-topic\n" }
     its(:stdout) { should include "parserConfig" }
     its(:stdout) { should include "SOFT_FAIL" }
   end
