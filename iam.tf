eed1b492897ce4b3ae7fbc4d6527825d2eec74d3
chaopeng <chaopeng@chaopeng.me>
2021-02-01 18:45:10 -0500

    remove test code

--- iam.tf
+++ iam.tf
@@ -93,15 +93,14 @@ resource "google_healthcare_hl7_v2_store_iam_member" "hl7_v2_store_iam_members" 
   member          = each.value.member
 }
 
-# resource "google_healthcare_consent_store_iam_member" "consent_store_iam_members" {
-#   provider = google-beta
-#   for_each = {
-#     for m in local.all_consent_iam_members :
-#     "${m.store_name} ${m.role} ${m.member}" => m
-#   }
-#   consent_store_id = google_healthcare_consent_store.consent_stores[each.value.store_name].name
-#   dataset          = google_healthcare_consent_store.consent_stores[each.value.store_name].dataset
-#   role             = each.value.role
-#   member           = each.value.member
-# }
-
+resource "google_healthcare_consent_store_iam_member" "consent_store_iam_members" {
+  provider = google-beta
+  for_each = {
+    for m in local.all_consent_iam_members :
+    "${m.store_name} ${m.role} ${m.member}" => m
+  }
+  consent_store_id = google_healthcare_consent_store.consent_stores[each.value.store_name].name
+  dataset          = google_healthcare_consent_store.consent_stores[each.value.store_name].dataset
+  role             = each.value.role
+  member           = each.value.member
+}
