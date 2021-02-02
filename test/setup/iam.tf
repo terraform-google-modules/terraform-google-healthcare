dd64678db2869d1cb10e9eabeae89843c5d7c825
chaopeng <chaopeng@chaopeng.me>
2021-02-01 20:50:55 -0500

    add consent admin

--- test/setup/iam.tf
+++ test/setup/iam.tf
@@ -16,6 +16,7 @@
 
 locals {
   int_required_roles = [
+    "roles/healthcare.consentStoreAdmin",
     "roles/healthcare.datasetAdmin",
     "roles/healthcare.dicomStoreAdmin",
     "roles/healthcare.fhirStoreAdmin",
