78d2963ec1cc9d233cab40feb0589ddeb3a1ebf3
chaopeng <chaopeng@chaopeng.me>
2021-05-13 12:13:58 -0400

    fix bq table creation

--- examples/simple_example/main.tf
+++ examples/simple_example/main.tf
@@ -28,7 +28,7 @@ resource "google_bigquery_dataset" "example_dataset" {
 resource "google_bigquery_table" "example_table" {
   dataset_id = google_bigquery_dataset.example_dataset.dataset_id
   table_id   = "example_table"
-  project       = var.project
+  project    = var.project
 }
 
 module "pubsub" {
