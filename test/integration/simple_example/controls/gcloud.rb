c2ead6f0d40a136863c7befd8088d990de2855e8
巢鹏 <chaopeng@chaopeng.me>
2021-05-17 13:44:41 -0400

    feat: Add dicom bq support (#48)

--- test/integration/simple_example/controls/gcloud.rb
+++ test/integration/simple_example/controls/gcloud.rb
@@ -34,6 +34,21 @@ control "gcloud" do
     its(:stderr) { should eq "" }
     its(:stdout) { should include "example-dicom-a" }
     its(:stdout) { should include "example-dicom-b" }
+    its(:stdout) { should include "example-dicom-c" }
+  end
+
+  describe command("gcloud --project=#{attribute("project")} healthcare dicom-stores describe example-dicom-a --dataset=example-healthcare-dataset") do
+    its(:exit_status) { should eq 0 }
+    its(:stderr) { should eq "" }
+    its(:stdout) { should include "notificationConfig" }
+    its(:stdout) { should include "projects/#{attribute("project")}/topics/example-topic\n" }
+  end
+
+  describe command("gcloud beta --project=#{attribute("project")} healthcare dicom-stores describe example-dicom-c --dataset=example-healthcare-dataset") do
+    its(:exit_status) { should eq 0 }
+    its(:stderr) { should eq "" }
+    its(:stdout) { should include "streamConfigs" }
+    its(:stdout) { should include "bq://#{attribute("project")}.example_dataset.example_table" }
   end
 
   # FHIR stores
