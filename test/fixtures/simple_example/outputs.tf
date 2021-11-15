8cb68a590b590692eae215224f9f0f3cf475b7fa
Bharath KKB <bharathkrishnakb@gmail.com>
2021-11-14 20:55:26 -0600

    Update outputs.tf

--- test/fixtures/simple_example/outputs.tf
+++ test/fixtures/simple_example/outputs.tf
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-output "project_id" {
+output "project" {
   description = "The ID of the project in which resources are provisioned."
   value       = var.project_id
 }
