9cebcd47c3e08d673fc8007bf2d9059def26b06b
Joyce Zhou <joycezz@google.com>
2022-10-11 15:29:44 -0400

    feat: Add fhir notification configs

--- test/fixtures/simple_example/versions.tf
+++ test/fixtures/simple_example/versions.tf
@@ -17,7 +17,7 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
-    google      = ">= 3.67.0"
-    google-beta = ">= 3.67.0"
+    google      = ">= 4.39.0"
+    google-beta = ">= 4.39.0"
   }
 }
