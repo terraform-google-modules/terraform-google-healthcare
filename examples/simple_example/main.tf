9de996cb33a269ce55a06e9301e1af2a512b1f8c
Ning <zheningzhang@google.com>
2023-11-03 09:56:41 -0400

    feat: add support for lastUpdatedPartitionConfig (#88)

--- examples/simple_example/main.tf
+++ examples/simple_example/main.tf
@@ -98,6 +98,10 @@ module "healthcare" {
           dataset_uri = "bq://${var.project}.${google_bigquery_dataset.example_dataset.dataset_id}"
           schema_config = {
             recursive_structure_depth = 3
+            last_updated_partition_config = {
+              type          = "HOUR"
+              expiration_ms = 1000000
+            }
           }
         }
       }]
