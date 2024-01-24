34670c9edff1b0cad3947b1477b3d97c645c9d4d
Ning <zheningzhang@google.com>
2024-01-24 10:03:40 -0500

    feat: add support for enableHistoryModifications (#93)

--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.76.0"
+      version = ">= 5.13.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.76.0"
+      version = ">= 5.13.0"
     }
   }
 }
