e0ccb3a289d0fcfe90352609aab54d31c0ba3fda
yeweidaniel <yeweidaniel@hotmail.com>
2024-02-05 22:40:41 -0500

    Revert "feat: add support for enableHistoryModifications (#93)" (#99)

--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.13.0"
+      version = ">= 4.76.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 5.13.0"
+      version = ">= 4.76.0"
     }
   }
 }
